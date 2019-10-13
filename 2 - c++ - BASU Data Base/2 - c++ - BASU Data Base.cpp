c7e7c14e12f77ce6175dacfaf7c6bf0d6241bea6
re7za <syd.id7@gmail.com>
2019-10-13 20:14:57 +0330

    first commit

---
+++ 2 - c++ - BASU Data Base/2 - c++ - BASU Data Base.cpp
@@ -0,0 +1,57 @@
+﻿//syd reza qaemi
+//9712358033
+//TA Project
+//The Second Project
+//BASU Data Base
+
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <string>
+
+vector <Class> Database;
+
+struct Date
+{
+	unsigned short int Year;
+	unsigned short int Month;
+	unsigned short int Day;
+};
+struct Student
+{
+	string Firstname;
+	string Lastname;
+	unsigned long long int ID;
+	Date Birthday;
+	float Grade;
+};
+struct Class
+{
+	string ClassName;
+	float Average;
+	unsigned short int Capacity;
+	vector <Student> Data;
+};
+
+void Start();
+void SelectClass(string);
+void AddClass(string);
+void RemoveClass(string);
+void AddStudent(string, Date, unsigned long long int, float);
+void RemoveStudent(unsigned long long int);
+void Search(unsigned long long int);
+void Search(string, string);
+void ShowClass(string);
+void ShowAll();
+void SortByName();
+void SortByID();
+void Save();
+
+int main()
+{
+	
+
+	return 0;
+}
